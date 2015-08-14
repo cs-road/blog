8213759875deb2777c792fb483c60188ccc8f8f6
sherry <871045520@qq.com>
2015-08-14 13:06:14 +0800

    myblog

---
+++ js/blog.js
@@ -0,0 +1,43 @@
+		
+$("#postsizebtn").bind("click",function(){
+
+	$('.sidebar').removeClass('windowchangehidesidebar');
+	
+	if($(this).hasClass('fullscreen')){
+		$('.sidebar').removeClass('hidesidebar');
+		$(this).removeClass('fullscreen');
+	}else{
+		$('.sidebar').addClass('hidesidebar');
+		$(this).addClass('fullscreen');
+	}		
+});
+
+$('#article-category ul').bind("click",function(event){
+		$('.allstyle').hide();
+		$('.'+$(event.target).attr('id')).delay(50).fadeIn();
+});
+
+var postlistclickhandler = function anoymous (){		
+	 if ($(window).width() <= 768) {
+	  
+	  if($("#postsizebtn").hasClass('fullscreen')){
+		$('.sidebar').addClass('windowchangehidesidebar');
+	  }
+			  
+	  $('.sidebar').addClass('mobile');
+	  $('.post-list').bind("click",function(){
+		$('.sidebar').addClass('hidesidebar');			
+		$('#postsizebtn').addClass('fullscreen');
+	  });	
+	}else{
+	
+		$('.sidebar').removeClass('windowchangehidesidebar');
+		
+		$('.sidebar').removeClass('mobile');
+		$('.post-list').unbind("click");
+	} 
+	return anoymous;
+	
+}();
+
+$(window).resize(postlistclickhandler);
